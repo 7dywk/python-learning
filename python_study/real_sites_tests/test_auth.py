dae96ecbbfbe5098ddb8656529ecd458cc96fad6
7dywk <illkryt321@gmail.com>
2026-05-17 20:27:00 +0200

    test: add negative login scenarios for wrong username and password

--- python_study/real_sites_tests/test_auth.py
+++ python_study/real_sites_tests/test_auth.py
@@ -11,3 +11,25 @@ def test_successful_login():
         login_page.login("tomsmith","SuperSecretPassword!")
         expect(login_page.flash_message).to_contain_text("You logged into")
         browser.close()
+
+
+def test_wrong_password_login():
+    with sync_playwright() as p:
+        browser = p.chromium.launch()
+        page = browser.new_page()
+        login_page = LoginPage(page)
+        login_page.navigate()
+        login_page.login("tomsmith","wrong password!")
+        expect(login_page.flash_message).to_contain_text("password is invalid")
+        browser.close()
+
+
+def test_wrong_username_login():
+    with sync_playwright() as p:
+        browser = p.chromium.launch()
+        page = browser.new_page()
+        login_page = LoginPage(page)
+        login_page.navigate()
+        login_page.login("wrong username","SuperSecretPassword!")
+        expect(login_page.flash_message).to_contain_text("username is invalid")
+        browser.close()
