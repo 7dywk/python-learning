8a1932146cd97bbac5548c6799ece87022d9fa44
7dywk <illkryt321@gmail.com>
2026-06-08 21:03:11 +0200

    feat: add safe price extraction and calculate mean property price

--- python_study/basics/json_check.py
+++ python_study/basics/json_check.py
@@ -7,7 +7,20 @@ properties = [
     {"id": "olx_106", "city": "Lublin"}
 ]
 
-for prop in properties:
-    details = prop.get("details", {})
-    price = details.get("price", 0)
-    print(price)
+def get_price(products):
+    final_prices = []
+    for prop in products:
+        details = prop.get("details", {})
+        price = details.get("price", 0)
+        final_prices.append(price)
+    return final_prices
+
+prices = get_price(properties)
+print(prices)
+
+clean_prices = [price for price in prices if price > 0]
+
+print(clean_prices)
+
+mean_price = sum(clean_prices) / len(clean_prices)
+print(f'Mean price: {mean_price:.2f}')
