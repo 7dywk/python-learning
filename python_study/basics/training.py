f10e390aafdf6deaf96150d120a4abe1aa1a8714
7dywk <illkryt321@gmail.com>
2026-07-01 20:49:48 +0200

    practice: REST API data filtering and CSV export

--- python_study/basics/training.py
+++ python_study/basics/training.py
@@ -4,27 +4,46 @@
 Виводить суму всіх цих транзакцій
 '''
 
-transactions = [
-    {"hash": "0xabc", "value": 5.2, "from": "0x111", "to": "0x222"},
-    {"hash": "0xdef", "value": 0.3, "from": "0x333", "to": "0x111"},
-    {"hash": "0xghi", "value": 12.7, "from": "0x444", "to": "0x555"},
-    {"hash": "0xjkl", "value": 1.1, "from": "0x111", "to": "0x666"},
-    {"hash": "0xmno", "value": 8.9, "from": "0x777", "to": "0x222"},
-]
+# transactions = [
+#     {"hash": "0xabc", "value": 5.2, "from": "0x111", "to": "0x222"},
+#     {"hash": "0xdef", "value": 0.3, "from": "0x333", "to": "0x111"},
+#     {"hash": "0xghi", "value": 12.7, "from": "0x444", "to": "0x555"},
+#     {"hash": "0xjkl", "value": 1.1, "from": "0x111", "to": "0x666"},
+#     {"hash": "0xmno", "value": 8.9, "from": "0x777", "to": "0x222"},
+# ]
+#
+#
+#
+# def get_info(transaction, address):
+#     sum_value = 0
+#     tran_list = []
+#     for item in transaction:
+#         if item['from'] == address or item['to'] == address:
+#             sum_value += item['value']
+#             tran_list.append(item)
+#     for tran in tran_list:
+#         print(f'Transactions with address {address}: {tran}')
+#     return f'sum value is {sum_value:.2f}'
+#
+# result = get_info(transactions ,"0x222")
+# print(result)
 
 
+import requests
+import csv
 
-def get_info(transaction, address):
-    sum_value = 0
-    tran_list = []
-    for item in transaction:
-        if item['from'] == address or item['to'] == address:
-            sum_value += item['value']
-            tran_list.append(item)
-    for tran in tran_list:
-        print(f'Transactions with address {address}: {tran}')
-    return f'sum value is {sum_value:.2f}'
 
-result = get_info(transactions ,"0x222")
-print(result)
+response = requests.get("https://jsonplaceholder.typicode.com/users")
+result = response.json()
+final_list = []
+
+filtered = [item for item in result if item["email"].endswith(".biz")]
+
+with open("../test_data/users.csv", "w", newline="") as f:
+    writer = csv.writer(f)
+    writer.writerow(["name", "email"])
+    for item in filtered:
+        writer.writerow([item["name"], item["email"]])
+
+
 
